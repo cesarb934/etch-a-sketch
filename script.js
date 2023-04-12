aa4e8e3285dbbde1fd4dedde2c3c29fee16ae5fa
Cesar <cesarb934@hotmail.com>
2023-04-11 23:13:56 -0300

    Add change size button and painting on hover

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+let gridSize = 16;
+
 let grid = document.getElementById('grid-container');
 
+let sizeButton = document.getElementById('size-button');
+sizeButton.addEventListener('click', changeGridSize);
 
-let columns=16;
-let rows = 16;
 
-
-for (let x = 0; x < columns; ++x) {
-    let column = document.createElement('div'); // create column
-    column.classList.add('column');
-    for (let y = 0; y < rows; ++y) {
-        let row = document.createElement('div'); // create row
-        row.classList.add('row' , 'block');
-        column.appendChild(row); // append row in column
+function changeGridSize(){
+    removeAllChildNodes(grid);
+    let newSize = prompt('Size must be between 1 and 100');
+    if (isNaN(newSize) || newSize < 1 || newSize > 100){
+        alert('Invalid input');
+        return;
+    }else{
+        gridSize = newSize;
+        createGrid();
     }
-    grid.appendChild(column); // append column inside grid
+
 }
-document.body.appendChild(grid);
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createGrid(){
+    for (let x = 0; x < gridSize; ++x) {
+        let column = document.createElement('div'); // create column
+        column.classList.add('column');
+        for (let y = 0; y < gridSize; ++y) {
+            let block = document.createElement('div'); // create row
+            block.classList.add('block');
+            block.addEventListener('mouseover', () => { block.style.backgroundColor = ' black' });
+            column.appendChild(block); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
+    }
+}
+
+createGrid();
